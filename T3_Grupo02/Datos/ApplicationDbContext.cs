6b4b674ef72cdf3ab104b2377911328b89ce38aa
Juu1302 <N00219842@upn.pe>
2024-11-08 23:38:02 +0100

    Conexion con la Base de Datos

---
+++ T3_Grupo02/Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using T3_Grupo02.Models;
+
+namespace T3_Grupo02.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options) 
+        {
+            
+        }
+        public DbSet<Libro> Libro { get; set; }
+    }
+}
--- T3_Grupo02/Models/Libro.cs
+++ T3_Grupo02/Models/Libro.cs
@@ -6,12 +6,28 @@ namespace T3_Grupo02.Models
     {
         [Key]
         public int id {  get; set; }
+
+        [Required(ErrorMessage ="El Titulo del libro es obligatorio")]
         public string titulo { get; set; }
+
+        [Required(ErrorMessage = "El Autor del libro es obligatorio")]
         public string autor { get; set; }
+
+        [Required(ErrorMessage = "El Tema del libro es obligatorio")]
         public string tema { get; set; }
+
+        [Required(ErrorMessage = "El Editorial del libro es obligatorio")]
         public string editorial { get; set; }
+
+        [Required(ErrorMessage = "El Año de publicacion es obligatorio")]
+        [Range(1900, 3000, ErrorMessage = "El Año debe ser de entre 1900 a 3000")]
         public int aniopublicacion { get; set; }
+
+        [Required(ErrorMessage = "Las paginas son obligatorio")]
+        [Range(10, 1000, ErrorMessage = "El libro debe tener de entre 10 a 1000 paginas")]
         public int paginas { get; set; }
+
+        [Required(ErrorMessage = "La categoria del libro es obligatoria")]
         public string categoria { get; set; }
 
     }

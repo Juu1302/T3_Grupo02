cf8a2ee245dad5bb578ba07416d17711c1be39ca
KalebFloresR <N00274951@upn.pe>
2024-11-08 18:14:12 -0500

    Controller completo

--- T3_Grupo02/Controllers/LibrosController.cs
+++ T3_Grupo02/Controllers/LibrosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using T3_Grupo02.Datos;
 using T3_Grupo02.Models;
 
@@ -16,6 +17,89 @@ namespace T3_Grupo02.Controllers
         {
             IEnumerable<Libro> lista = _db.Libro;
             return View(lista);
+        }
+
+        [Authorize]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Libro.Add(libro);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libro);
+
+        }
+
+        [Authorize]
+        //Editar
+        public IActionResult Editar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Libro.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Libro.Update(libro);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libro);
+
+        }
+
+
+        [Authorize]
+        //Eliminar
+        public IActionResult Eliminar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Libro.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Eliminar(Libro libro)
+        {
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            _db.Libro.Remove(libro);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+
         }
     }
 }

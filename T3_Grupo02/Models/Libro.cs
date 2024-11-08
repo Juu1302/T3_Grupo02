bb3ce64020f46d67a98ac4671b84a55167c6ac41
Lee Akio Bruno Mauricio Taboada <n00295053@upn.pe>
2024-11-08 22:49:43 +0100

    modelo modificado

--- T3_Grupo02/Models/Libro.cs
+++ T3_Grupo02/Models/Libro.cs
@@ -1,6 +1,17 @@
-﻿namespace T3_Grupo02.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace T3_Grupo02.Models
 {
     public class Libro
     {
+        [Key]
+        public int id {  get; set; }
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public string tema { get; set; }
+        public string editorial { get; set; }
+        public int aniopublicacion { get; set; }
+        public int paginas { get; set; }
+
     }
 }
